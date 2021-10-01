b2c101bf5eccf4fcf1a7efae012eab5f86bd8a6f
Yureche <yurigerber.g18@gmail.com>
2021-10-01 22:01:49 +0300

    Added form to create books

--- app.js
+++ app.js
@@ -1,50 +1,43 @@
 // ! Variables
-const addBookButton = document.querySelector('.add-button');
-const mainContainer = document.querySelector('.main-container');
-let library = []
+const mainContainer = document.querySelector(".main-container");
+let library = [];
+// Create Book Form
+const form = document.querySelector(".form-container");
+const closeFormButton = document.querySelector(".close-form");
+const openFormButton = document.querySelector(".open-form");
+const overlay = document.querySelector(".overlay");
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function createBook(bookTitle) {
-    let titleInput = prompt("What is the book title: ");
-    let authorInput = prompt("Who is the book author: ");
-    let pagesInput = prompt("How many pages? ");
-    let readInput = prompt("Have you read the book yet?");
-    bookTitle = new Book(titleInput, authorInput, pagesInput, readInput);
-    library.push(bookTitle);
-
-    
-    let bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    mainContainer.appendChild(bookCard);
-    let title = document.createElement('h1');
-    title.classList.add('book-title');
-    bookCard.appendChild(title);
-    title.innerHTML = `${bookTitle.title}`;
-    let author = document.createElement('h3');
-    author.classList.add('author');
-    author.innerHTML = `Author: ${bookTitle.author}`;
-    bookCard.appendChild(author);
-    let pages = document.createElement('h4');
-    pages.classList.add('pages');
-    pages.innerHTML = `Pages: ${bookTitle.pages}`;
-    bookCard.appendChild(pages);
-    let readLabel = document.createElement('h4');
-    readLabel.classList.add('read-label');
-    readLabel.innerHTML = "Have you read the book? ";
-    bookCard.appendChild(readLabel);
-    let readCheckBox = document.createElement('input');
-    readCheckBox.type = 'checkbox';
-    readCheckBox.classList.add('readInput');
-    readLabel.appendChild(readCheckBox);
+function openForm() {
+  if (form.style.display === "flex") {
+    return;
+  }
+  form.style.display = "flex";
+  overlay.style.opacity = "1";
+  overlay.style.pointerEvents = "all";
 }
 
-addBookButton.addEventListener('click', () => {
-    createBook("Hobbit");
-})
+function closeForm() {
+  form.style.display = "none";
+  overlay.style.opacity = "0";
+  overlay.style.pointerEvents = "none";
+}
 
+openFormButton.addEventListener("click", () => {
+  openForm();
+});
+
+closeFormButton.addEventListener("click", () => {
+  closeForm();
+});
