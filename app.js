72eb6d8e09ffc2fe9da61c28745d4ccc90aa9bbf
Yureche <yurigerber.g18@gmail.com>
2021-10-05 22:57:49 +0300

    Added form validation

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 // ! Variables
 const mainContainer = document.querySelector(".main-container");
 let library = [];
-// Create Book Form
-const form = document.querySelector(".form-container");
-const closeFormButton = document.querySelector(".close-form");
+// Buttons to open and close the form
 const openFormButton = document.querySelector(".open-form");
-const overlay = document.querySelector(".overlay");
+const closeFormButton = document.querySelector(".close-form");
 
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-const read = document.querySelector("#read");
+const createBookButton = document.querySelector("#submit-book");
+// Create book Form
+const overlay = document.querySelector(".overlay");
+const formContainer = document.querySelector(".form-container");
+
+const inputFields = document.querySelectorAll(".input:not(#img-url)");
+
+// ! Validating the form
+const form = document.querySelector("form");
+function validate(e) {
+  inputFields.forEach((input) => {
+    if (input.value.trim() == "") {
+      // Add a class that defines an animation
+      input.classList.add("error");
+      // remove the class after the animation completes
+      setTimeout(function () {
+        input.classList.remove("error");
+      }, 300);
+    }
+  });
+
+  e.preventDefault();
+}
+
+form.addEventListener("submit", validate);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -20,24 +39,24 @@ function Book(title, author, pages, read) {
 }
 
 function openForm() {
-  if (form.style.display === "flex") {
-    return;
-  }
-  form.style.display = "flex";
-  overlay.style.opacity = "1";
+  formContainer.style.display = "flex";
+  overlay.style.opacity = 1;
   overlay.style.pointerEvents = "all";
 }
 
 function closeForm() {
-  form.style.display = "none";
-  overlay.style.opacity = "0";
+  formContainer.style.display = "none";
+  overlay.style.opacity = 0;
   overlay.style.pointerEvents = "none";
 }
-
 openFormButton.addEventListener("click", () => {
   openForm();
 });
 
 closeFormButton.addEventListener("click", () => {
   closeForm();
+});
+
+createBookButton.addEventListener("click", () => {
+  // createBook();
 });
